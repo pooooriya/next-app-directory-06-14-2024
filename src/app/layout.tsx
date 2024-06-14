94ea0c6ef7bdd671c3b56273facdce2fb76b7dba
pooooriya <pouriya.babaali1998@gmail.com>
2024-06-14 15:21:39 +0330

    first-commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
+import Layout from "@/components/layout";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fa" dir="rtl">
+      <body>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
---
+++ src/app/product/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "@/components/layout";
+
+const Product = () => {
+  return <>Product</>;
+};
+
+export default Product;
